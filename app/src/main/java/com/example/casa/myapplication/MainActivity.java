164dfcf84b7fe10b0c1d80f4a6f04c7896a57ec6
Jean Vitor <jeanvitordepaulo@gmail.com>
2016-10-04 22:15:12 -0300

    Teste

--- app/src/main/java/com/example/casa/myapplication/MainActivity.java
+++ app/src/main/java/com/example/casa/myapplication/MainActivity.java
@@ -2,9 +2,22 @@ package com.example.casa.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    static{
+        if (OpenCVLoader.initDebug()){
+            Log.d(TAG, "Opencv Loaded");
+        } else{
+            Log.d(TAG, "ERRO");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
