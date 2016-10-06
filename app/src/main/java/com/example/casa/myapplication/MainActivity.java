d878956990e509857c2d83702dcfa9cb19f4a8a4
Jean Vitor <jeanvitordepaulo@gmail.com>
2016-10-06 00:11:38 -0300

    Teste

--- app/src/main/java/com/example/casa/myapplication/MainActivity.java
+++ app/src/main/java/com/example/casa/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.casa.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     static{
+        System.loadLibrary("MyLibs");
         if (OpenCVLoader.initDebug()){
             Log.d(TAG, "Opencv Loaded");
         } else{
@@ -23,5 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.textView)).setText(NativeClass.getMessageFromJNI());
     }
 }
