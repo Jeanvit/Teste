61061e71781ade8afb60e3cb69ac4a2d968e4377
Jean Vitor <jeanvitordepaulo@gmail.com>
2016-10-11 17:38:24 -0300

    Verificando

--- app/src/main/java/com/example/casa/myapplication/MainActivity.java
+++ app/src/main/java/com/example/casa/myapplication/MainActivity.java
@@ -3,28 +3,86 @@ package com.example.casa.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "MainActivity";
+    JavaCameraView javaCameraView;
+    Mat mRgba;
+    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
 
-    static{
-        System.loadLibrary("MyLibs");
-        if (OpenCVLoader.initDebug()){
-            Log.d(TAG, "Opencv Loaded");
-        } else{
-            Log.d(TAG, "ERRO");
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:
+                    javaCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
         }
-    }
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((TextView) findViewById(R.id.textView)).setText(NativeClass.getMessageFromJNI());
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(View.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (javaCameraView!=null)
+            javaCameraView.disableView();
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        if (javaCameraView!=null)
+            javaCameraView.disableView();
+
+    }
+    protected void onResume(){
+        super.onResume();
+        if (OpenCVLoader.initDebug()){
+            Log.i(TAG,"OpenCV Loaded sucessfully");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }else{
+            Log.i(TAG, "OpenCV Error");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0,this, mLoaderCallback);
+        }
+
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return mRgba;
     }
 }
